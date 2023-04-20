d7141c1167f4e335a36f050b9426881bd5a509f4
jch1999 <54787512+jch1999@users.noreply.github.com>
2023-04-20 14:08:17 +0000

    파일 다운로드 기능 구현 Implementation of file download functionality

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, send_file
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
+from extractors.gameJob import extract_gameJobs_jobs
+from file import save_to_file
 
 app = Flask("JobScrapper")
+
+db = {}
 
 
 @app.route("/")
@@ -8,9 +14,35 @@ def home():
   return render_template("home.html", name="chanho")
 
 
-@app.route("/hello")
+@app.route("/search")
 def hello():
-  return 'hello you!'
+  keyword = request.args.get("keyword")
+  if keyword == None:
+    return redirect("/")
+  if keyword == "":
+    return redirect("/")
+  if keyword in db:
+    jobs = db[keyword]
+  else:
+    indeed = extract_indeed_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    #gameJob = extract_gameJobs_jobs(keyword)
+    jobs = indeed + wwr  # + gameJob
+    db[keyword] = jobs
+  return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+  keyword = request.args.get("keyword")
+  if keyword == None:
+    return redirect("/")
+  if keyword == "":
+    return redirect("/")
+  if keyword not in db:
+    return redirect(f"/search?keyword={keyword}")
+  save_to_file(keyword, db[keyword])
+  return send_file(f"{keyword}.csv", as_attachment=True)
 
 
 app.run("0.0.0.0")
